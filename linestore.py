541233b160e5f9793b3bb724ecf57b66ff5e1b1b
dedeibel <dedeibel@arcor.de>
2018-11-18 21:00:02 +0100

    Improve linestore, add int and long storage, multiple record storage

--- linestore.py
+++ linestore.py
@@ -1,8 +1,15 @@
 import os
-from struct import pack, unpack
+from struct import pack, unpack, calcsize
 
 def _hex(data):
-    return ''.join(str(i) for i in data)
+    return ''.join(("%02x" % ord(i)) for i in data)
+
+def _starts(buff, search):
+    slen = len(search)
+    if len(buff) < slen:
+        return False
+
+    return buff[:slen] == search
 
 # tab sep floats
 class Linestore:
@@ -34,8 +41,20 @@ class Linestore:
         self.fp.write(pack("H", version))
         self.fp.write(b"\x02")
         for val in data:
-            self.fp.write(pack("f", val))
-            self.fp.write(b"\x1E")
+            if type(val) == float:
+                self.fp.write('f')
+                self.fp.write(pack("f", val))
+                self.fp.write(b'\x1E')
+            elif type(val) == int:
+                self.fp.write('i')
+                self.fp.write(pack("i", val))
+                self.fp.write(b'\x1E')
+            elif type(val) == long:
+                self.fp.write('l')
+                self.fp.write(pack("l", val))
+                self.fp.write(b'\x1E')
+            else:
+                print('ERR unsupported type: ' + str(type(val)))
         self.fp.write(b"\xFF\x1D\xFF")
         self.fp.flush()
         # gibt version aus
@@ -51,8 +70,9 @@ class Linestore:
 
         while True:
             chunk = self.fp.read(Linestore.CHUNK_SIZE)
-            print("chunk len: " + str(len(chunk)))
-            print("to read: " + _hex(chunk))
+            #print("chunk len: " + str(len(chunk)))
+            #print("to read: " + _hex(chunk))
+
             if chunk == b"":
                 break
             elif len(chunk) < 9:
@@ -60,61 +80,81 @@ class Linestore:
                 break
 
             index = 0
-            if chunk[index:index + 3] == b"\xFF\x00\xFF":
-                index += 3
-                try:
-                    print("idx read: "+ _hex(chunk[index:index+2]))
-                    found_version = unpack("H", chunk[index:index+2])[0]
-                    parsed_version = int(found_version)
-                    if parsed_version != version:
+            while index < len(chunk):
+                if _starts(chunk[index:], b'\xFF\x00\xFF'):
+                    index += 3
+                    try:
+                        #print("idx read: "+ _hex(chunk[index:index+2]))
+                        found_version = unpack("H", chunk[index:index+2])[0]
+                        parsed_version = int(found_version)
+                        if parsed_version != version:
+                            break
+
+                        index += 2
+
+                        if not _starts(chunk[index:], b"\x02"):
+                            print("version not properly terminated")
+                            break
+
+                        index += 1
+                    except Exception as e:
+                        print("illegal version found: " + str(e))
                         break
-
-                    index += 2
-
-                    if chunk[index:index + 1] != b"\x02":
-                        print("version not properly terminated")
-                        break
-
+                else:
+                    # keep seaching for the start
                     index += 1
-                except Exception as e:
-                    print("illegal version found: " + str(e))
-                    break
-
-            current_entry = []
-            entry_index = index
-            while entry_index < len(chunk):
-                print("chunk size: "+ str(len(chunk)) +" index: " + str(index) + " entry_idx: "+ str(entry_index))
-
-                if chunk[entry_index:entry_index + 3] == b"\xFF\x1D\xFF":
-                    results.append(current_entry)
-                    current_entry = []
-                    entry_index += 3
                     continue
 
-                # no float to read
-                if (len(chunk) - entry_index) < 4:
-                    break
+                current_entry = []
+                entry_index = index
 
-                print("to read: " + _hex(chunk))
-                print("val: "+ str(float(unpack('f', chunk[entry_index:entry_index + 4])[0])))
+                while entry_index < len(chunk):
+                    #print("chunk size: "+ str(len(chunk)) +" index: " + str(index) + " entry_idx: "+ str(entry_index))
 
-                unpacked = float(unpack('f', chunk[entry_index:entry_index + 4])[0])
-                entry_index += 4
+                    if _starts(chunk[entry_index:], b"\xFF\x1D\xFF"):
+                        results.append(current_entry)
+                        current_entry = []
+                        entry_index += 3
+                        break
 
-                if chunk[entry_index:entry_index + 1] != b"\x1E":
-                    print("entry not properly terminated")
-                    break
+                    # no data plus markers to read
+                    if (len(chunk) - entry_index) < 4:
+                        break
 
-                entry_index += 1
+                    data_type = chunk[entry_index:entry_index + 1]
+                    #print("type: "+ _hex(data_type))
 
-                current_entry.append(unpacked)
+                    entry_index += 1
 
+                    #print("to read: " + _hex(chunk[entry_index:]))
 
-            # search for version (buffer 2)
-            # read entries, split at RS
-            # stop at GS
-            #parsed_group = self._parse_group(line)
-            #results.append(parsed_group)
+                    unpacked_data = None
+                    if data_type == 'f':
+                        unpacked_data = float(unpack('f', chunk[entry_index:entry_index + calcsize('f')])[0])
+                        entry_index += calcsize('f')
+                    elif data_type == 'i':
+                        unpacked_data = int(unpack('i', chunk[entry_index:entry_index + calcsize('i')])[0])
+                        entry_index += calcsize('i')
+                    elif data_type == 'l':
+                        unpacked_data = long(unpack('l', chunk[entry_index:entry_index + calcsize('l')])[0])
+                        entry_index += calcsize('l')
+                    else:
+                        print('ERR unsupported type: ' + _hex(data_type))
+
+                    #print("val: "+ str(unpacked_data))
+
+                    if not _starts(chunk[entry_index:], b"\x1E"):
+                        print("entry not properly terminated")
+                        break
+                    entry_index += 1
+
+                    current_entry.append(unpacked_data)
+
+                # search for version (buffer 2)
+                # read entries, split at RS
+                # stop at GS
+                #parsed_group = self._parse_group(line)
+                #results.append(parsed_group)
 
         return results
 
--- linestore_test.py
+++ linestore_test.py
@@ -22,7 +22,7 @@ class LinestoreTest(unittest.TestCase):
         with TempFilepath() as path:
             store = Linestore(path)
             store.open()
-            store.append(1, [2.0, 3.0])
+            store.append(1, [2.0, 3])
 
             with open(path, "rb") as tmpfile:
                 content = tmpfile.read(32)
@@ -31,13 +31,13 @@ class LinestoreTest(unittest.TestCase):
                 # version 
                 self.assertEqual(content[3:6], "\x01\x00\x02");
                 # first value 
-                self.assertEqual(content[6:11], "\x00\x00\x00@\x1E");
+                self.assertEqual(content[6:12], "f\x00\x00\x00@\x1E");
                 # second value 
-                self.assertEqual(content[11:16], "\x00\x00@@\x1E");
+                self.assertEqual(content[12:18], "i\x03\x00\x00\x00\x1E");
                 # group end marker
-                self.assertEqual(content[16:19], "\xFF\x1D\xFF");
+                self.assertEqual(content[18:21], "\xFF\x1D\xFF");
 
-                self.assertEqual(len(content), 19);
+                self.assertEqual(len(content), 21);
 
     def test_validate_version(self):
         with TempFilepath() as path:
@@ -49,32 +49,95 @@ class LinestoreTest(unittest.TestCase):
             result = store.readlines(2)
             self.assertEqual(result, [])
 
-    def test_read(self):
+    def test_write_read_float(self):
         with TempFilepath() as path:
             store = Linestore(path)
             store.open()
-            store.append(1, [2.0, 3.0])
+            store.append(1, [2.0, 3.2, 1./3.])
             store.close()
             store.open()
             result = store.readlines(1)
-            self.assertEqual(result, [[2.0, 3.0]])
+            self.assertEqual(len(result), 1)
+            self.assertEqual(len(result[0]), 3)
+
+            self.assertEqual(result[0][0], 2.0)
+            self.assertAlmostEqual(result[0][1], 3.2)
+            self.assertAlmostEqual(result[0][2], 1./3.)
+
+            self.assertEqual(type(result[0][0]), float)
+            self.assertEqual(type(result[0][1]), float)
+            self.assertEqual(type(result[0][2]), float)
+
+    def test_write_read_int(self):
+        with TempFilepath() as path:
+            store = Linestore(path)
+            store.open()
+            store.append(1, [5, -4, 0])
+            store.close()
+            store.open()
+            result = store.readlines(1)
+            self.assertEqual(len(result), 1)
+            self.assertEqual(len(result[0]), 3)
+            self.assertEqual(type(result[0][0]), int)
+            self.assertEqual(type(result[0][1]), int)
+            self.assertEqual(type(result[0][2]), int)
+            self.assertEqual(result, [[5,-4,0]])
+
+    def test_write_read_long(self):
+        with TempFilepath() as path:
+            store = Linestore(path)
+            store.open()
+            store.append(1, [9223372036854775807L, -4L, 0L])
+            store.close()
+            store.open()
+            result = store.readlines(1)
+            self.assertEqual(len(result), 1)
+            self.assertEqual(len(result[0]), 3)
+            self.assertEqual(type(result[0][0]), long)
+            self.assertEqual(type(result[0][1]), long)
+            self.assertEqual(type(result[0][2]), long)
+            self.assertEqual(result, [[9223372036854775807L, -4L, 0L]])
+
+    def test_write_read_mixed(self):
+        with TempFilepath() as path:
+            store = Linestore(path)
+            store.open()
+            store.append(1, [9223372036854775807L, 3.1415, 12])
+            store.close()
+            store.open()
+            result = store.readlines(1)
+            self.assertEqual(len(result), 1)
+            self.assertEqual(len(result[0]), 3)
+            self.assertEqual(type(result[0][0]), long)
+            self.assertEqual(type(result[0][1]), float)
+            self.assertEqual(type(result[0][2]), int)
+
+            self.assertEqual(result[0][0], 9223372036854775807L)
+            self.assertAlmostEqual(result[0][1], 3.1415)
+            self.assertEqual(result[0][2], 12)
+
+    def test_write_read_multiple_lines(self):
+        with TempFilepath() as path:
+            store = Linestore(path)
+            store.open()
+            store.append(1, [23, 1.23])
+            store.append(1, [24, 2.151])
+            store.append(1, [5, 0.0014])
+            store.close()
+            store.open()
+            result = store.readlines(1)
+            self.assertEqual(len(result), 3)
+            self.assertEqual(len(result[0]), 2)
+            self.assertEqual(len(result[1]), 2)
+            self.assertEqual(len(result[2]), 2)
+
+            self.assertEqual(result[0][0], 23)
+            self.assertAlmostEqual(result[0][1], 1.23)
+
+            self.assertEqual(result[1][0], 24)
+            self.assertAlmostEqual(result[1][1], 2.151, 4)
+
+            self.assertEqual(result[2][0], 5)
+            self.assertAlmostEqual(result[2][1], 0.0014, 5)
 
 
-#    def test_read_floats(self):
-#        with TempFilepath() as path:
-#            spit(path, "1\t2.0\t3.4\n"+
-#                       "1\t4.2\t5.3\n")
-#            store = Linestore(path)
-#            store.open()
-#            read = store.readlines(1)
-#            self.assertEqual(read, [[1.0,2.0,3.4],[1.0,4.2,5.3]]);
-#
-#    def test_read_int(self):
-#        with TempFilepath() as path:
-#            spit(path, "1\t2\t3\n"+
-#                       "1\t4\t5\n")
-#            store = Linestore(path)
-#            store.open()
-#            read = store.readlines(1)
-#            self.assertEqual(read, [[1.0,2.0,3.4],[1.0,4.2,5.3]]);
-#
