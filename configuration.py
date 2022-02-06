f6c81b93baffa78bd2603dbd23391b2d79ef24aa
Benjamin Peter <mail@benjaminpeter.name>
2022-02-06 21:27:29 +0100

    Use MS directly for deepsleep time

--- configuration.py
+++ configuration.py
@@ -2,24 +2,24 @@
 DHT22_PIN = const(12)
 TEMP_SENSOR_PIN = const(13)
 
-#ENTRIES_SEND_BATCH_SIZE = const(1)
+ENTRIES_SEND_BATCH_SIZE = const(1)
 # -1 means no deepsleep, should not be smaller than send batch size
 # or the data is lost
-#DEEPSLEEP_AFTER_ITERATIONS = const(1)
+DEEPSLEEP_AFTER_ITERATIONS = const(1)
 # Irrelevant if deepsleep after iterations is 1
-#SECONDS_BETWEEN_MEASUREMENTS = const(900) # 15 minutes
+SECONDS_BETWEEN_MEASUREMENTS = const(900) # 15 minutes
 
 # Max is about 3:30h https://thingpulse.com/max-deep-sleep-for-esp8266/
 # 298: 5 Minutes, minus 2 seconds startup
-#DEEPSLEEP_SECONDS = const(898) # 15 minutes
+DEEPSLEEP_MS = const(898000) # 15 minutes
 
 ERROR_TIMEOUT_HOLDOFF_MAX_SECONDS = const(60)
 
 # Settings for debugging
-ENTRIES_SEND_BATCH_SIZE = const(1)
-SECONDS_BETWEEN_MEASUREMENTS = const(15)
-DEEPSLEEP_AFTER_ITERATIONS = const(3)
-DEEPSLEEP_SECONDS = const(20)
+#ENTRIES_SEND_BATCH_SIZE = const(1)
+#SECONDS_BETWEEN_MEASUREMENTS = const(15)
+#DEEPSLEEP_AFTER_ITERATIONS = const(3)
+#DEEPSLEEP_MS = const(20000)
 
 NTP_UPDATE_TIME = True
 
