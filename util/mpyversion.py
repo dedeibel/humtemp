3b692c8ea00b289afe5915337d7f25b732f40e94
Benjamin Peter <mail@benjaminpeter.name>
2023-08-06 14:12:46 +0200

    Update mpyversion.py to micropython 20

--- util/mpyversion.py
+++ util/mpyversion.py
@@ -1,5 +1,5 @@
 import sys
-sys_mpy = sys.implementation.mpy
+sys_mpy = sys.implementation._mpy
 arch = [None, 'x86', 'x64',
     'armv6', 'armv6m', 'armv7m', 'armv7em', 'armv7emsp', 'armv7emdp',
     'xtensa', 'xtensawin'][sys_mpy >> 10]
